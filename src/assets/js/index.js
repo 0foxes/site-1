89c9ee3dbde797207711bd1616fc037c70b93ef8
Computer_Genius <31290848+Computer-Genuis@users.noreply.github.com>
2020-03-24 08:21:35 -0400

    yeet

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -125,25 +125,15 @@ function SHA256(s) {
     return binb2hex(core_sha256(str2binb(s), s.length * chrsz));
 }
 
-// Set the date we're counting down to
-var countDownDate = new Date("Mar 25, 2020 00:00:00").getTime();
-
-// Update the count down every 1 millisecond
+// Update the count down every 100 milliseconds
 var x = setInterval(function() {
 
     // Get todays date and time
     var now = new Date().getTime();
-
-    // Find the distance between now and the countdown date in milliseconds
-    var distance = String(countDownDate - now);
+    var distance = String(now);
 
     // Hash the result and display it in the element with id="countdown"
     hash = SHA256(distance)
-    document.getElementById("countdown").innerHTML = hash
+    document.getElementById("hash").innerHTML = hash
 
-    // If the countdown is finished, write some text
-    if (distance <= 0) {
-        clearInterval(x);
-        document.getElementById("countdown").innerHTML = "Main Site Launching Soon";
-    }
 }, 100);
